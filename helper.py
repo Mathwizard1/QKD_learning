217aee4c829e89706c42767e169233bc8edb0300
Mathwizard1 <anshurup.gupta@gmail.com>
2025-03-03 04:50:55 +0530

    B92 notebook

--- helper.py
+++ helper.py
@@ -2,7 +2,10 @@ import pickle as pk
 from qiskit import QuantumCircuit
 
 message_bits = (0, 1)
+empty = -1
 possible_bases = ['H', 'D']
+
+possible_gates = ['x', 'h', 'z', 'cx']
 
 def encrypter_decrypter(A = "", L = []):
     if(L == []):
@@ -71,7 +74,8 @@ class QQ2grader:
         self.rollno = rollno
 
     def display(self):
-        print(f"{self.name} Advanced to next level.\nCurrent score: {self.__score}")
+        print(f"{self.name} Advanced to next level.\nCurrent score: {self.__score} / 6")
+        self.file_dump()
 
     def intro_message(self):
         print(f'''
@@ -83,9 +87,9 @@ All The best!
 ''')
 
     def file_dump(self):
-        if(self.__answer_list != {}):
-            with open(answer_file, "wb") as fp:
-                pk.dump(fp, self.__answer_list)
+        self.__answer_list[0] = (self.name, self.rollno)
+        with open(answer_file, 'wb') as fp:
+            pk.dump(self.__answer_list, fp)
 
     def check_task(self, obj, i):
         if(self.__task_flag):
@@ -98,10 +102,66 @@ All The best!
         print("Your score: ", self.__score)      
 
     def Q1(self, qc: QuantumCircuit, bits: list):
-        pass
+        self.__task_flag = True
+
+        qcInstructions = qc.data
+
+        # Only barrier is wrong if 1 present
+        if(len(qcInstructions) == 1 and 1 in bits):
+            self.__task_flag = False
+
+        for qcInstruction in qcInstructions:
+            # print(qcInstruction)
+
+            if(qcInstruction.name == possible_gates[1]):
+                qIndex = qcInstruction.qubits[0]._index
+                # print(qIndex)
+
+                if(bits[qIndex] != message_bits[1]):
+                    print('Wrong encoding.')
+                    self.__task_flag = False
+                    break
+            elif(qcInstruction.name != 'barrier'):
+                print(f'Wrong {qcInstruction.name} gate.')
+                self.__task_flag = False
+                break
+
+        self.check_task((bits), 1)
 
     def Q2(self, qc: QuantumCircuit, bases: list):
-        pass
+        self.__task_flag = True
+
+        temp_bases = list([empty] * len(bases))
+        barrier_found = False
+        qcInstructions = qc.data
+
+        for qcInstruction in qcInstructions:
+            # print(qcInstruction)
+
+            if(qcInstruction.name == 'barrier' or barrier_found):
+                if(qcInstruction.name == possible_gates[1]):
+                    qIndex = qcInstruction.qubits[0]._index
+                    # print(qIndex)
+
+                    if(bases[qIndex] != possible_bases[1] or temp_bases[qIndex] == bases[qIndex]):
+                        self.__task_flag = False
+                        break
+                    temp_bases[qIndex] = possible_bases[1]
+
+                elif(qcInstruction.name != 'barrier'):
+                    print(f'Wrong {qcInstruction.name} gate.')
+                    self.__task_flag = False
+                    break
+
+                if(not barrier_found):
+                    barrier_found = True
+                elif(qcInstruction.name == 'barrier' and barrier_found):
+                    if(possible_bases[1] in bases and possible_bases[1] not in temp_bases):
+                        print("Error in bases and gates")
+                        self.__task_flag = False
+                    break
+
+        self.check_task((bases), 2)
 
     def Q3(self, count_dict: dict, key_string: str):
         key_strings = tuple(count_dict.keys())
@@ -114,10 +174,45 @@ All The best!
         self.check_task((key_strings, key_string), 3)
 
     def Q4(self, unity_indexes: list, bits: list):
-        pass
+        self.__task_flag = True
+
+        if(len(unity_indexes) == 0):
+            print("Re-run for Non-empty u_index key.")
+            self.__task_flag = False
+
+        for index in unity_indexes:
+            if(index > len(bits) or bits[index] == empty):
+                self.__task_flag = False
+                break
+
+        self.check_task((unity_indexes, bits), 4)
 
     def Q5(self, function):
-        pass
+        self.__task_flag = True
+        test_case1 = [0,0,0,0,0]
+        test_case2 = [1,1,1,1,1]
+        test_case3 = [0,0,0,0,1]
+        test_case4 = [0,1,0,1,0]
 
-    def Q6(self):
-        pass
+        tests = (
+            (test_case1, test_case1, True),
+            (test_case2, test_case2, True),
+            (test_case1, test_case3, True),
+            (test_case2, test_case4, False),
+            (test_case3, test_case4, False)
+        )
+
+        for test_case in tests:
+            A,B,V = test_case
+            if(function(A, B) != V):
+                self.__task_flag = False
+                break
+
+        self.check_task((), 5)
+
+    def Q6(self, key_1: list, key_2: list):
+        if(key_1 == key_2):
+            self.__task_flag = True
+        else:
+            self.__task_flag = False
+        self.check_task((key_1, key_2), 6)
